3ed75eff3bdd4223f69ae7e8b1e1d459bff64af8
Bryce Jobs <lecebri@gmail.com>
2015-03-14 14:34:18 +0530

    updating validation for firstname and email

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ActiveRecord::Base
-	validates :first_name, presence:true
+	validates :first_name, presence:true, length: {in: 4..25}
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
